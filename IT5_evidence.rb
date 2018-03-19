b7b6fdf08e0f5281ae88247580346fc0c641154b
rach-j <35229623+rach-j@users.noreply.github.com>
2018-03-19 09:54:52 +0000

    Delete IT5_evidence.rb
    Added in error.

--- IT5_evidence.rb
+++
@@ -1,8 +0,0 @@
-cheesecake_ingredients = ["biscuits", "butter", "cream cheese", "sugar", "double cream", "raspberries"]
-
-def display_ingredients(ingredients)
-  p "The ingredients in this dish are:"
-  ingredients. each {|ingredient| p ingredient}
-end
-
-display_ingredients(cheesecake_ingredients)
